2c7212329a2829acdde5534f6ae046d9ac5c3d04
Luis <lgmarchioreh@gmail.com>
2023-12-11 10:31:33 -0300

    FIX(user)  Fixed the error to update the password on the user serializer

--- users/serializers.py
+++ users/serializers.py
@@ -25,8 +25,12 @@ class UserSerializer(serializers.Serializer):
 
     def update(self, instance: User, validated_data: dict) -> User:
         for key, value in validated_data.items():
-            setattr(instance, key, value)
+            if key == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
 
         instance.save()
 
         return instance
+

340d26c5871b67a97d24364dfcc244f785584eba
Luis <lgmarchioreh@gmail.com>
2023-12-11 11:53:56 -0300

    refactor(songs) Refactored the songSerializer

--- songs/serializers.py
+++ songs/serializers.py
@@ -1,13 +1,12 @@
 from rest_framework import serializers
-
+from albums.serializers import AlbumSerializer
 from .models import Song
 
-
-class SongSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(max_length=255)
-    duration = serializers.CharField(max_length=255)
-    album_id = serializers.IntegerField(read_only=True)
-
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ["id", "album_id", "title", "duration"]
+        depth = 1
+        
     def create(self, validated_data):
         return Song.objects.create(**validated_data)

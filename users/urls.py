af153ef39084da7b0a7214e210d2ead09758aa26
Luis <lgmarchioreh@gmail.com>
2023-12-11 15:55:10 -0300

    refactor(users) Refactored all users routes

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path("users/", views.CreateUserView.as_view()),
+    path("users/", views.PostUserView.as_view()),
     path("users/<int:pk>/", views.UserDetailView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
 ]

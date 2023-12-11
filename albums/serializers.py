d7690ab84c69edf20a297b5358335aac4e3e16ba
Luis <lgmarchioreh@gmail.com>
2023-12-11 11:07:29 -0300

    refactor(albuns) Refactored the albumSerializer

--- albums/serializers.py
+++ albums/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 from .models import Album
 from users.serializers import UserSerializer
 
-
-class AlbumSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    year = serializers.IntegerField()
+class AlbumSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Album
+        fields = "__all__"
 
     def create(self, validated_data):
         return Album.objects.create(**validated_data)

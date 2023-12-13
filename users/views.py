4e3c2196859830c9b0b571284a08eaa818509aed
Luis <lgmarchioreh@gmail.com>
2023-12-13 12:40:00 -0300

    REFACTOR(albums) Refactored album routes

--- users/views.py
+++ users/views.py
@@ -2,13 +2,13 @@ from .models import User
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
 from .permissions import IsAccountOwner
-from rest_framework.generics import CreateAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 
 class PostUserView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class UserDetailView(RetrieveAPIView, DestroyAPIView, UpdateAPIView):
+class UserDetailView(RetrieveUpdateDestroyAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwner]
 
